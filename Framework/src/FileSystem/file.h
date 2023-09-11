073a3fa42a85c233dfcdb57877605eccbdf80d18
Roland <roland.brannon.am@gmail.com>
2023-09-11 22:58:07 +0200

    Added binary read and write feature

--- Framework/src/FileSystem/file.h
+++ Framework/src/FileSystem/file.h
@@ -1,11 +1,28 @@
 #pragma once
 #include <string>
+#include <vector>
+#include <span>
 
 namespace Ff
 {
 	std::string readFileToString(const std::string& path);
+
 	void writeStringToFile(const std::string& path, const std::string& stringToWrite);
-
 	
+	std::vector<std::byte> readBinaryFile(const std::string& path);
 
+	template<typename T>
+	T readBinaryFile(const std::string& path)
+	{
+		std::vector<std::byte> bytes = readBinaryFile(path);
+		return *reinterpret_cast<T*>(bytes.data());
+	}
+	
+	void writeToBinaryFile(const std::string& path, const void* data, uint64_t size);
+
+	template<typename T>
+	void writeToBinaryFile(const std::string& path, const T& object)
+	{
+		writeToBinaryFile(path, &object, sizeof(object));
+	}
 }
