f151126e21f6036a89d24732c091536ad2670789
Roland <roland.brannon.am@gmail.com>
2023-10-08 19:41:31 +0200

    Model view matrix, MVP prototype

--- Framework/Frameworkf/src/framework.cpp
+++ Framework/Frameworkf/src/framework.cpp
@@ -8,6 +8,7 @@
 #include "graphicsContext.h"
 #include "Shader/shaderProgram.h"
 #include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 #include "Buffer/buffer.h"
 #include "renderer.h"
 #include "Texture/texture.h"
@@ -78,8 +79,11 @@ int main()
 
     init();
 
-    glfwSwapInterval(1);
-    float x{};
+    glfwSwapInterval(2);
+    float x = 0.5f;
+    float y = 0.7f;
+    float z{};
+
     float increment = 0.05f;
 
     while (!window.shouldClose())
@@ -88,6 +92,17 @@ int main()
         Viewport2D viewport{};
         viewport.width = window.getWidth();
         viewport.height = window.getHeight();
+
+        float aspectRatio = viewport.width / viewport.height;
+        float displayWidth = 4.0f;
+        float displayHeight = displayWidth / aspectRatio;
+
+        const float halfDisplayWidth = displayWidth * 0.5f;
+        const float halfDisplayHeight = displayHeight * 0.5f;
+
+        glm::mat4 proj = glm::ortho(-halfDisplayWidth, halfDisplayWidth, -halfDisplayHeight, halfDisplayHeight, -1.0f, 1.0f);
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), -glm::vec3(1.0f, 0.0f, 0.0f));
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
         update();
 
@@ -98,7 +113,9 @@ int main()
         context->bindTexture(texture);
         context->setConstant("u_Texture", int32_t(0));
         context->setViewport(viewport);
-        context->setConstant("u_Color", glm::vec4(x, 0.5f, 0.5f, 1.0f));
+        context->setConstant("u_Color", glm::vec4(x, y, 0.5f, 1.0f));
+
+        context->setConstant("u_MVP", proj * view * model);
 
         renderer.draw(vertexBuffer, indexBuffer);
 
@@ -109,6 +126,12 @@ int main()
         else if (x < 0.0f)
             increment = 0.05f;
         x += increment;
+
+        if (y > 1.0f)
+            increment = -0.05f;
+        else if (y < 0.0f)
+            increment = 0.05f;
+        y += increment;
 
         window.swapBuffers();
     }
--- Framework/Shaders/testVS.glsl
+++ Framework/Shaders/testVS.glsl
@@ -3,10 +3,12 @@ layout (location = 0) in vec3 Position;
 layout (location = 1) in vec3 Color;
 layout (location = 2) in vec2 UV;
 
+uniform mat4 u_MVP;
+
 out vec2 pass_UV;
 
 void main()
 {
-    gl_Position = vec4(Position, 1.0);
+    gl_Position = u_MVP * vec4(Position, 1.0);
     pass_UV = UV;
 }
